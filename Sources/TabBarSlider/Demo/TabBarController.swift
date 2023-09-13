59db36b6bb3a3603fab1d6a3b77e466b74b92f97
Paul Popa <paul.popa@outlook.com>
2023-09-13 23:47:48 +0100

    Configure custom tab bar and utilise delegate method for selecting each tab

--- Sources/TabBarSlider/Demo/TabBarController.swift
+++ Sources/TabBarSlider/Demo/TabBarController.swift
@@ -52,3 +52,58 @@ class NewTabBarViewController: UITabBarController, TabControllerProtocol {
         setupTransitionCoordinator()
     }
 }
+
+
+//MARK: - TabBar Configuration
+
+extension NewTabBarViewController {
+        
+    private func setupTabs() {
+        
+        let homeTabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        let chatTabBarItem = UITabBarItem(title: "Chat", image: UIImage(systemName: "paperplane"), tag: 1)
+        let searchTabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        
+        let leafTabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "leaf"), tag: 3)
+        
+        homeVC.tabBarItem = homeTabBarItem
+        homeVC.topViewController?.title = "Home"
+        view.backgroundColor = .systemGray6
+        
+        chatVC.tabBarItem = chatTabBarItem
+        chatVC.view.backgroundColor = .systemGray5
+        chatVC.topViewController?.title = "Chat"
+        
+        searchVC.tabBarItem = searchTabBarItem
+        searchVC.view.backgroundColor = .systemGray4
+        searchVC.topViewController?.title = "Search"
+        
+        leafVC.tabBarItem = leafTabBarItem
+        leafVC.view.backgroundColor = .systemGray4
+        leafVC.topViewController?.title = "Green"
+        
+        viewControllers = [homeVC,chatVC,searchVC,leafVC]
+        self.selectedIndex = 0
+        
+        customTabBar.items = [homeTabBarItem,chatTabBarItem,searchTabBarItem,leafTabBarItem]
+    }
+    
+    //MARK: - Constraints
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+
+        switch true {
+            
+        case item.title == "Home":
+            selectedViewController = homeVC
+        case item.title == "Chat":
+            selectedViewController = chatVC
+        case item.title == "Search":
+            selectedViewController = searchVC
+        case item.title == "Green":
+            selectedViewController = leafVC
+        default:
+            break
+        }
+    }
+}
