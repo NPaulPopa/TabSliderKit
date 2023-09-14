cbe2c3de9d6a24e3de056b3eff09f6075b95db0e
Paul Popa <paul.popa@outlook.com>
2023-09-14 23:31:39 +0100

    Create demo implementation of HomeViewController adopting FloatingButtonViewControllerProtocol

---
+++ Sources/TabBarSlider/Demo/HomeViewController.swift
@@ -0,0 +1,65 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paul on 13/09/2023.
+//
+
+import UIKit
+import Combine
+import Extensions
+import MiniItemBasket
+import FloatingButton
+
+protocol HomeViewControllerProtocol: AnyObject {
+    
+    func shouldAddChildViewController(parent: UIViewController)
+    func shouldRemoveChildViewController(parent: UIViewController)
+}
+
+class HomeViewController: ProgrammaticViewController, FloatingButtonViewControllerProtocol {
+    
+    weak var delegate: HomeViewControllerProtocol?
+                
+    lazy var floatingTableView: FloatingButtonScrollViewProtocol = FloatingButtonTableView()
+        
+    var capsuleFloatingButton: UIView!
+    
+    override var childForStatusBarHidden: UIViewController? {
+        let child = children.last
+        return child
+    }
+    override var prefersStatusBarHidden: Bool { true }
+    private var cancellables = Set<AnyCancellable>()
+        
+    init(){
+        super.init(nibName: nil, bundle: nil)
+        setupFloatingButton()
+    }
+    
+    override func configure() {
+        title = "Home"
+                
+        subscribeToTransitionPublisher()
+        subscribeToNotificationCenterPublisher()
+    }
+    
+    override func constrain() {
+
+        addConstrainedSubview(floatingTableView)
+        pinToBounds(parent: view, subview: floatingTableView)
+
+        //TODO: - Include the lines below inside HomeCollectionViewController
+        constrainFloatingButton()
+        delegate?.shouldAddChildViewController(parent: self)
+    }
+        
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.setNeedsLayout()
+    }
+    
+
+}
+
+
