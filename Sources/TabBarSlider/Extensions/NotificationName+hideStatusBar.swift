8a8cf03b252132381dbf15b61f6d84dfd4a37a69
Paul Popa <paul.popa@outlook.com>
2023-09-24 23:51:51 +0100

    Create notification for updating the CustomTabBar & documentation on how to use it

--- Sources/TabBarSlider/Extensions/NotificationName+hideStatusBar.swift
+++ Sources/TabBarSlider/Extensions/NotificationName+hideStatusBar.swift
@@ -27,3 +27,29 @@ public extension Notification.Name {
     static let shouldHideStatusBar = Notification.Name("ShouldHideStatusBarNotification")
 }
 
+
+public extension Notification.Name {
+    
+    ///Subscribe to this notification inside TabBarController object to change the TabBarContainer's bounds with sliding animation by applying CGAffineTransform
+    ///
+    ///
+    ///```
+    ///  func subscribeToNotifications() {
+    ///    NotificationCenter.default.publisher(for: .updateTabBarTransform)
+    ///        .compactMap { $0.object as? SheetState }
+    ///        .sink { [weak self] sheetState in
+    ///
+    ///            guard let self = self,
+    ///                  //tabBarController must conform to TabControllerProtocol
+    ///                  let tabBarViewController = tabBarController,
+    ///                  let tabBarContainer = tabBarViewController.tabBarContainer else { return }
+    ///
+    ///            tabBarContainer.transform = sheetState == .closed ? .identity : CGAffineTransform(translationX: 0, y: tabBarContainer.bounds.height)
+    ///        }
+    ///        .store(in: &cancellables)
+    /// }
+    ///```
+    /// - This notification is sent whenever the MiniPlayer is changing its state from .closed to .opened and vice-versa
+    ///
+    static let updateTabBarTransform = Notification.Name("ShouldUpdateTabBarNotification")
+}
