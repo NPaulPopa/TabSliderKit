17aa92df70769b9887a396b28d60045cd5f7838a
Paul Popa <paul.popa@outlook.com>
2023-09-24 23:58:37 +0100

    Create protocol and custom implementation for setting up the transitionManager

---
+++ Sources/TabBarSlider/TabBarProtocol/ParentControllerProtocol.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paul on 24/09/2023.
+//
+
+import Foundation
+import UIKit
+
+public protocol ParentControllerProtocol {
+    
+    var screenSize: CGFloat { get }
+    var transitionManager: TransitionManager! { get set }
+    
+    mutating func setupTransitionCoordinator(playerViewController: MiniPlayerProtocol)
+}
+
+public extension ParentControllerProtocol {
+    
+    var screenSize: CGFloat {
+        UIScreen.main.bounds.height
+    }
+    
+    mutating func setupTransitionCoordinator(playerViewController: MiniPlayerProtocol) {
+
+        let noSafeAreaDevice = screenSize < 670
+        let bottomInset: CGFloat = noSafeAreaDevice ? 62 : 27
+        let withSafeAreaInset: CGFloat = 83 + bottomInset
+        let withoutSafeAreInset: CGFloat = 49 + bottomInset
+
+        let additionalBottomInset = noSafeAreaDevice ? withoutSafeAreInset : withSafeAreaInset
+        
+        playerViewController.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: additionalBottomInset, right: 0)
+
+        transitionManager = TransitionManager(playerViewController: playerViewController)
+    }
+}
