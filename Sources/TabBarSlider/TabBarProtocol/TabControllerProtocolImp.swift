43b01c9245e49cd0d02ce12d0148b2d20202a944
Paul Popa <paul.popa@outlook.com>
2023-09-24 23:56:39 +0100

    Remove transitionCoordinator dependencies and its setupTransitionCoordinator() method

--- Sources/TabBarSlider/TabBarProtocol/TabControllerProtocolImp.swift
+++ Sources/TabBarSlider/TabBarProtocol/TabControllerProtocolImp.swift
@@ -24,36 +24,7 @@ public extension TabControllerProtocol {
         constrainCustomTabBar()
         configureTabBarAppearance()
     }
-    
-    func setupTransitionCoordinator() {
         
-       // let tabBarBottomSafeAreaInset = customTabBar.safeAreaInsets.bottom
-     //  let bottomInset: CGFloat = tabBarBottomSafeAreaInset == 0 ? 62 : 27
-     //   let additionalBottomInset = customTabBar.bounds.height + bottomInset
-        
-        let noSafeAreaDevice = screenSize < 670
-        let bottomInset: CGFloat = noSafeAreaDevice ? 62 : 27
-        let withSafeAreaInset: CGFloat = 83 + bottomInset
-        let withoutSafeAreInset: CGFloat = 49 + bottomInset
-
-        let additionalBottomInset = noSafeAreaDevice ? withoutSafeAreInset : withSafeAreaInset
-        
-        playerViewController.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: additionalBottomInset, right: 0)
-
-        //TODO: - `The following actions
-        /*
-         1. Remove tabBarViewController dependency from TransitionManager
-         
-         2. Pass playerViewController into setupTransitionCoordinator
-         - I need to find a way to provide the customTabBar's bounds or bottom SafeArea insets in order to add the bottom offset as UIEdgInsets on the playerViewController
-         
-         - The goal is to remove the playerViewController and coordinator dependency so that this extension and its corresponding protocol is only responsible for adding a custom tab bar and a custom container
-         
-         - The object who knows about both the coordinator and the playerViewController should be the object that presents the playerViewController
-         */
-        coordinator = TransitionManager(playerViewController: playerViewController)
-    }
-    
     func constrainCustomTabBar() {
                         
         tabBarContainer.translatesAutoresizingMaskIntoConstraints = false
